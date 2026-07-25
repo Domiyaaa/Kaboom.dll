1f20ac10226e0dfda2d176a57ca83236c6e6c525
Domiyaaa <podolskidominik9@gmail.com>
2026-07-25 19:48:56 -0400

    penis

---
+++ Kaboom.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace Kaboom
+{
+    [BepInPlugin("Domiyaa.Kaboom", "Kaboom", "1.0.0")]
+    public class InstantQuitPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            float roll = UnityEngine.Random.Range(0f, 1f);
+
+            if (roll < 0.2f)
+            {
+                Logger.LogInfo($"LetsGoGambling: {roll:F3}  awh dang it ");
+
+                UnityEngine.Application.Quit();
+
+#if UNITY_STANDALONE
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+#endif
+            }
+            else
+            {
+                Logger.LogInfo($"LetsGoGambling: {roll:F3} yay!");
+            }
+        }
+    }
+}
